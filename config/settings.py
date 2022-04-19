29b7e356b426a57ddc9a3bf5c497a29f1bcab046
윤혜린 <1234youn2@gmail.com>
2022-04-20 00:05:51 +0900

    heroku upload

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'yaou-p2v#__p3+ak$5)9uhbj(7jg)3hnkx6fcra_lsk$w1g^!!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com']
 
